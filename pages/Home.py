b6aa79c2d4419c019c4c13cc3f4557a5c49cabfd
ahmadkayyali-snowflake <ahmad.al-kayyali@snowfkake.com>
2025-02-18 16:12:00 +1100

    Changed info to home

--- pages/Home.py
+++ pages/Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title(":information_source: Information")
+st.title(":snowboarder: Home")
 
 st.markdown("---")
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ st.set_page_config(page_title="", page_icon="🔍", layout="wide")
 
 pages = {
     "Navigation Pages": [
+        st.Page("pages/Home.py", title="Home"),
         st.Page("pages/Prospect_Finder.py", title="Prospect Finder"),
         st.Page("pages/Message_Generation.py", title="Message Generation"),
-        st.Page("pages/Information.py", title="Information"),
     ]
 }
 pg = st.navigation(pages)
